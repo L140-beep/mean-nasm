7626b383a6271701ae3d3e18b0dc8224f8854eeb
L140-beep <kartoshka0302@mail.ru>
2022-10-06 16:21:35 +0800

    added minus sign

--- main.asm
+++ main.asm
@@ -20,7 +20,7 @@
 %macro const_print 1
     pushd
     jmp %%print 
-    %%str db %1, 0xA
+    %%str db %1
     %%len equ $ - %%str
 %%print:  
     mov eax, 4
@@ -33,10 +33,10 @@
 
 %macro print 2
     pushd
+    mov eax, 4
+    mov ebx, 1
     mov edx, %1
     mov ecx, %2
-    mov ebx, 1
-    mov eax, 4
     int 0x80
     popd
 %endmacro
@@ -124,6 +124,7 @@ _sum_y:
     jg _ifgrower
 
 _iflower:
+    mov [sign], byte 0
     mov eax, [x_sum]
     mov ebx, [y_sum]
     sub eax, ebx
@@ -131,6 +132,7 @@ _iflower:
     jmp _result
 
 _ifgrower:
+    mov [sign], byte 1
     mov eax, [y_sum]
     mov ebx, [x_sum]
     sub eax, ebx
@@ -138,12 +140,19 @@ _ifgrower:
     jmp _result
 
 
-
 _result:
     mov ebx, array_len
     div ebx
     const_print "Среднее арифметическое разности двух массивов: "
+    new_line
+    cmp [sign], byte 1 
+    je _minus
+    jmp _print
+_minus: 
+    putchar '-'
+_print:
     printd
+
     putchar ','
     decimal edx, array_len
     printd
@@ -154,15 +163,18 @@ _result:
 
     EXIT 0
 
+
 section .data
-    x dd 25, 3, 2, 6, 1, 7, 4 ;48
-    y dd 0, 10, 1, 9, 2, 8, 5 ; 35
+    x dd 25, 3, 2, 6, 1, 7, 4 
+    y dd 0, 10, 1, 9, 2, 8, 5 
     array_len equ ($ - y) / 4
     message db "Done!"
     len equ $ - message
+
 section .bss
     result resb 1
     x_sum resb 10
     y_sum resb 10
     result_integer resb 5
     result_decimal resb 1
+    sign resb 1
